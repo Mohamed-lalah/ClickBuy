2638c8b6290dae2d2f3d41268ebc321069fd9b15
Mohamed-lalah <mohamedlalah918@gmail.com>
2024-07-08 23:49:49 +0300

    class app colors added

--- lib/utilites/app_colors.dart
+++ lib/utilites/app_colors.dart
@@ -7,5 +7,7 @@ abstract  class AppColors {
   static const Color grey = Color(0xffC8C9CB);
   static const Color white= Colors.white;
   static const Color transparent= Colors.transparent;
+  static const Color primiary= Color(0xff5D9CEC);
+  static const Color selectedColor = Color(0xff61E757);
 
 }
