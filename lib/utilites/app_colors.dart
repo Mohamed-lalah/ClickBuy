3ff06347d4f907f9d30085e2a31c1c90837e8821
Mohamed-lalah <mohamedlalah918@gmail.com>
2024-07-08 23:36:17 +0300

    class app colors added

---
+++ lib/utilites/app_colors.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+abstract  class AppColors {
+  static const Color black= Colors.black;
+  static const Color grey = Color(0xffC8C9CB);
+  static const Color white= Colors.white;
+  static const Color transparent= Colors.transparent;
+
+}
